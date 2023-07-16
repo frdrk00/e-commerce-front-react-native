702187ad998440b06d5679e1fa6fcc8033bf835b
frdrk00 <masterkooo@gmail.com>
2023-07-16 13:34:06 +0200

    Setup

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-blue-400">Home ❤</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
